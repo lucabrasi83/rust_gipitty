7db34ac2d797ea0ecbce541e0b56b7dbe5550637
Sebastien Pouplin <seb.pouplin@gmail.com>
2023-12-29 15:06:17 +0700

    Added Depedencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use actix_cors::Cors;
+use actix_web::{http::header, web, App, HttpResponse, HttpServer, Responder};
+use async_trait::async_trait;
+use reqwest::Client as HttpClient;
+use serde::{Deserialize, Serialize};
+
+use std::collections::HashMap;
+use std::fs;
+use std::io::Write;
+use std::sync::Mutex;
+
 fn main() {
     println!("Hello, world!");
 }
