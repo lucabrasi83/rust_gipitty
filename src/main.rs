5637067d6a0f9176a38ed8a9fdc7883307a6eaa2
Sebastien Pouplin <seb.pouplin@gmail.com>
2023-12-29 20:29:37 +0700

    Basic web server setup

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,110 @@ use std::fs;
 use std::io::Write;
 use std::sync::Mutex;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool,
+}
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>,
+}
+
+impl Database {
+    fn new() -> Self {
+        Self {
+            tasks: HashMap::new(),
+            users: HashMap::new(),
+        }
+    }
+
+    // CRUD DATA
+    fn insert(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
+    fn get(&mut self, id: &u64) -> Option<&Task> {
+        self.tasks.get(id)
+    }
+    fn get_all(&mut self) -> Vec<&Task> {
+        self.tasks.values().collect()
+    }
+    fn delete(&mut self, id: &u64) {
+        self.tasks.remove(id);
+    }
+    fn update(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
+
+    // USER DATA
+    fn insert_user(&mut self, user: User) {
+        self.users.insert(user.id, user);
+    }
+
+    fn get_user_by_name(&self, username: &str) -> Option<&User> {
+        self.users.values().find(|u| u.username == username)
+    }
+    fn save_to_file(&self) -> std::io::Result<()> {
+        let data = serde_json::to_string(self)?;
+        let mut file = fs::File::create("database.json")?;
+
+        file.write_all(data.as_bytes())?;
+        Ok(())
+    }
+
+    fn load_from_file() -> std::io::Result<Self> {
+        let file_content = fs::read_to_string("database.json")?;
+        let db: Database = serde_json::from_str(&file_content)?;
+        Ok(db)
+    }
+}
+
+struct AppState {
+    db: Mutex<Database>,
+}
+
+async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder {
+    let mut db = app_state.db.lock().unwrap();
+    db.insert(task.into_inner());
+    let _ = db.save_to_file();
+    println!("{:?}", db);
+    HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = match Database::load_from_file() {
+        Ok(db) => db,
+        Err(_) => Database::new(),
+    };
+
+    let data = web::Data::new(AppState { db: Mutex::new(db) });
+
+    HttpServer::new(move || {
+        App::new().wrap(
+            Cors::permissive()
+                .allowed_origin_fn(|origin, _req_head| {
+                    origin.as_bytes().starts_with(b"http://localhost") || origin.is_empty()
+                })
+                .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                .allowed_header(header::CONTENT_TYPE)
+                .max_age(3600)
+        )
+        .app_data(data.clone())
+        .route("/task", web::post().to(create_task))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+
 }
